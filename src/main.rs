e3d61375434aa2ae15dfdf0233c74a8ac3e665a4
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-02-22 09:35:55 -0700

    update src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {secret_number}");
-    
     loop {
         println!("Please input your guess.");
 
@@ -28,7 +26,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
